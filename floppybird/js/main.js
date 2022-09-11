4730be7d9c491689a3d846cd266ac480d74654d2
ChengHe <chobocho99@gmail.com>
2022-09-11 10:21:28 +0900

    [Code] Update message

--- floppybird/js/main.js
+++ floppybird/js/main.js
@@ -4,16 +4,20 @@ function OnDraw() {
 }
 
 function processEvent(code) {
+  printf("[Main] processEvent: ", code);
   switch (code) {
     case 32:
     case 38:
-      console.log("Up");
+      printf("[Main] processEvent: ", "UP");
       gameEngine.moveUp();
       break;
-    case 80: console.log("Pause");
+    case 27:
+    case 80:
+      printf("[Main] processEvent: ", "Pause");
       gameEngine.pause();
       break;
-    case 83: console.log("Start");
+    case 83:
+      printf("[Main] processEvent: ", "Start");
       gameEngine.start();
       break;
     default:
@@ -42,14 +46,16 @@ function InitCanvas() {
   canvas.width = 800;
   canvas.height = 600;
   cvs = canvas.getContext("2d");
-  log_msg = "Width: " + canvas.width + " Height: " + canvas.height;
-  printf("[main]", log_msg);
+  // log_msg = "Width: " + canvas.width + " Height: " + canvas.height;
+  // printf("[main]", log_msg);
 
   bufCanvas = document.createElement("canvas");
   bufCanvas.width = canvas.width;
   bufCanvas.height = canvas.height;
   bufCtx = bufCanvas.getContext("2d");
-  log_msg = "Width: " + bufCanvas.width + " Height: " + bufCanvas.height;
+
+  log_msg = "S: Start / SPACE, Up arrow: Jump / ESC, P: Pause  ";
+  log_msg += "[" + bufCanvas.width + "x" + bufCanvas.height + "]";
   printf("[main] bufCtx", log_msg);
   document.getElementById("message").innerHTML = log_msg;
 }
